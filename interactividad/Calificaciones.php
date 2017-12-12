c0b1e3770a64226b8d32b90ab788418cdedc6d16
Alejandro Castro Saucedo <Alejandro Castro Saucedo>
2017-12-12 14:15:42 -0600

    Ya cambie tu L julieta

--- interactividad/Calificaciones.php
+++ interactividad/Calificaciones.php
@@ -5,46 +5,16 @@ $baseDatos=mysql_select_db('controlescolar') or die ("Problemas al seleccionar l
 
 $query=mysql_query("SELECT cursos.periodo,materias.materia,docentes.nombre, cursos_alumnos.calificacion from docentes, materias, cursos, cursos_alumnos, alumnos WHERE alumnos.noCtrl=$noCtrl AND alumnos.noCtrl=cursos_alumnos.noCtrl AND materias.claveMateria=cursos.claveMateria AND cursos.idCurso=cursos_alumnos.idCurso AND cursos.idDocente=docentes.idDocente",$con);
 $response=array();
+$num=mysql_num_rows($query);
 while($fila = mysql_fetch_array($query)){
-	$response["succes"]=true;
-	$response["periodo"]=$fila['periodo'];
-	$response["materia"]=$fila['materia'];
-	$response["nombre"]=$fila['nombre'];
-	$response["calificacion"]=$fila['calificacion'];
+	$row_array["succes"]=true;
+	$row_array["periodo"]=$fila['periodo'];
+	$row_array["materia"]=$fila['materia'];
+	$row_array["nombre"]=$fila['nombre'];
+	$row_array["calificacion"]=$fila['calificacion'];
+	array_push ($response, $row_array);
 }
-
 echo json_encode($response);
 mysql_close();
-/*
-$con= mysqli_connect("localhost", "root", "", "controlescolar")or die("Imposible conectar ");
-//mysql_select_db("users")or die("cannot select DB");
-
-$noCtrl=$_REQUEST["noCtrl"];
-
-//$sql="SELCT * FROM usuarios WHERE username=? AND password=?";
-//$result=mysql_query($sql, $con) or die ('Unable to run query:'.mysql_error());
-
-/*$statement=mysqli_prepare($con, "SELECT alumnos.noCtrl, alumnos.nombre, alumnos.primerApellido,alumnos.segundoApellido, alumnos.grupo FROM usuariosalumnos,alumnos WHERE usuariosalumnos.noCtrl=? AND usuariosalumnos.password=? AND usuariosalumnos.noCtrl=alumnos.noCtrl");*/
-/*
-$statement=mysqli_prepare($con,"SELECT cursos.periodo,materias.materia,docentes.nombre, cursos_alumnos.calificacion from docentes, materias, cursos, cursos_alumnos, alumnos WHERE alumnos.noCtrl='?' AND alumnos.noCtrl=cursos_alumnos.noCtrl AND materias.claveMateria=cursos.claveMateria AND cursos.idCurso=cursos_alumnos.idCurso AND cursos.idDocente=docentes.idDocente");
-
-//mysqli_stmt_bind_param($statement, "s", $noCtrl);
-$execute=mysqli_stmt_execute($statement);
-echo $execute;
-/*mysqli_stmt_store_result($statement);
-mysqli_stmt_bind_result($statement, $periodo, $materia, $nombre, $calificacion);*/
-/*
-$response=array();
-$response["succes"]=false;
-
-while($fila = mysqli_fetch_row($execute)){
-	$response["succes"]=true;
-	$response["periodo"]=$fila['periodo'];
-	$response["materia"]=$fila['materia'];
-	$response["nombre"]=$fila['nombre'];
-	$response["calificacion"]=$fila['calificacion'];
-}
-
-echo json_encode($response);*/
 
 ?>
