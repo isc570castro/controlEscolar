5aa9fbd815df7109ef463244b80be65f9cc7c3f8
Alejandro Castro Saucedo <Alejandro Castro Saucedo>
2017-12-11 20:48:05 -0600

    Actualizacion

--- view/inicio.php
+++ view/inicio.php
@@ -15,5 +15,59 @@
 	<center><h1> Bienvenido al sistema de control escolar</h1>
 		<h4>Seleccione el curso con el que necesite trabajar</h4>
 	</center><br>
-	
+	 <div class="row">
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-aqua">
+            <div class="inner">
+               <h3 style="margin-top: 20px; margin-bottom:20px;">Docentes</h3>
+            </div>
+            <div class="icon">
+              <i class="ion ion-person"></i>
+            </div>
+            <a href="?c=Docente" class="small-box-footer">Ir a vista <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-green">
+            <div class="inner">
+              <h3 style="margin-top: 20px; margin-bottom:20px;">Alumnos</h3>
+            </div>
+            <div class="icon">
+              <i class="ion ion-person-add"></i>
+            </div>
+            <a href="?c=Alumno" class="small-box-footer">Ir a vista <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-yellow">
+            <div class="inner">
+               <h3 style="margin-top: 20px; margin-bottom:20px;">Materias</h3>
+            </div>
+            <div class="icon">
+              <i class="ion ion-stats-bars"></i>
+            </div>
+            <a href="?c=Materia" class="small-box-footer">Ir a vista <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-red">
+            <div class="inner">
+              <h3 style="margin-top: 20px; margin-bottom:20px;">Cursos</h3>
+            </div>
+            <div class="icon">
+              <i class="ion ion-bag"></i>
+            </div>
+            <a href="?c=Curso" class="small-box-footer">Ir a vista <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+      </div>
+      <!-- /.row -->
 </section>
